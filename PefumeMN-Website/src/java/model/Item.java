fd73277e798110bd183d80d9df8cd39963a1973e
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2023-12-22 18:04:34 +0700

    aa

---
+++ PefumeMN-Website/src/java/model/Item.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author lvhho
+ */
+public class Item {
+    // Thong tin mot sp trong gio hang
+}
